4fc0c2f54d77d761bb844d7dd3eac2f79c379924
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 10:42:03 +0530

    user implemented

--- src/main/java/org/example/user/Mahesh.java
+++ src/main/java/org/example/user/Mahesh.java
@@ -1,4 +1,25 @@
 package org.example.user;
 
+import jakarta.annotation.PostConstruct;
+import org.example.contract.Bakery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Mahesh {
+
+    private Bakery bakery;
+
+    @Autowired
+    @Qualifier("")
+    public void setGoodGirl(Bakery bakery) {
+        this.bakery = bakery;
+    }
+
+    @PostConstruct
+    public void init(){
+        bakery.consumption();
+    }
+
 }
