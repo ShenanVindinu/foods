5880874af74ba9a44aa0c79581b5708e00b62a20
ShenanVindinu <shenanvindinu@gmail.com>
2024-08-31 10:50:33 +0530

    annotaions added

--- src/main/java/org/example/beans/ChickenRoll.java
+++ src/main/java/org/example/beans/ChickenRoll.java
@@ -1,7 +1,13 @@
 package org.example.beans;
 
 import org.example.contract.Bakery;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary
+@Qualifier("")
 public class ChickenRoll implements Bakery {
     @Override
     public void consumption() {
--- src/main/java/org/example/beans/FishBun.java
+++ src/main/java/org/example/beans/FishBun.java
@@ -1,7 +1,12 @@
 package org.example.beans;
 
 import org.example.contract.Bakery;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Qualifier("primary")
 public class FishBun implements Bakery {
     @Override
     public void consumption() {
